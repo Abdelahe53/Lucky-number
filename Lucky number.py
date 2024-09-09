ea1b2c935b50af680f3c50de870e42103aec1726
GondalF <74936090+Abdelahe53@users.noreply.github.com>
2024-09-09 15:24:05 +0100

    While loop fixed

--- Lucky number.py
+++ Lucky number.py
@@ -1,10 +1,12 @@
 import random  # Import the "random" library
 
 print("If you get lucky 13 on the first try, you are the luckiest person!")  # Print a message
+user = input("Tap Enter...")
 
 lucky_num = 13
+user = 0
 
-while True:
+while user < lucky_num:
     random_number = random.randint(1, 100)  # Generate a random number between 1 and 100
     print("Your number is:", random_number)  # Display the random number
 
