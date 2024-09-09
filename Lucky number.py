a28faefc1bfdaca37fba9bd0792b8a610a10200c
GondalF <74936090+Abdelahe53@users.noreply.github.com>
2024-09-09 16:39:08 +0100

    added attempts

--- Lucky number.py
+++ Lucky number.py
@@ -1,12 +1,12 @@
 import random  # Import the "random" library
 
 print("If you get lucky 13 on the first try, you are the luckiest person!")  # Print a message
-user = input("Tap Enter...")
+input("3/3 Tap Enter...")
 
 lucky_num = 13
-user = 0
+attempts = 3
 
-while user < lucky_num:
+while attempts > 0:
     random_number = random.randint(1, 100)  # Generate a random number between 1 and 100
     print("Your number is:", random_number)  # Display the random number
 
@@ -14,4 +14,10 @@ while user < lucky_num:
         print("You're lucky ma boi!")
         break
     else:
-        input("99% of gamblers quit before they win big, try again...")
+        attempts -= 1
+        if attempts > 0:
+            input(f"{attempts}/3 attempts left try again...")
+
+if attempts == 0:
+    print("99% of gamblers quit before they win big, try again...")
+
